fa744111335799aaeb69bb9da28180f86856f41f
Mursal Rashik <mursalrashikk@gmail.com>
2025-03-25 14:04:28 +0530

    google auth callback url

--- config/passport.js
+++ config/passport.js
@@ -11,7 +11,7 @@ const env = require("dotenv").config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://trendora.ddns.net/auth/google/callback", 
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
 
 async (accessToken, refreshToken, profile, done)=>{
